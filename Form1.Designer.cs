034b3d50c9d30eac06d3e9520602c881bda2548d
Anthony Paz Coral <thony1124@gmail.com>
2025-10-13 20:19:49 +0200

    Anthony/13/10
    Primer Merge

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
+            panel1 = new Panel();
+            buttonInicioTareas = new Button();
+            button1 = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -40,24 +43,46 @@
             label1.Size = new Size(0, 15);
             label1.TabIndex = 0;
             // 
-            // label2
+            // panel1
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(10, 11);
-            label2.Name = "label2";
-            label2.Size = new Size(42, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Beatrix";
+            panel1.BackColor = Color.FromArgb(41, 192, 205);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(buttonInicioTareas);
+            panel1.ForeColor = SystemColors.ControlText;
+            panel1.ImeMode = ImeMode.NoControl;
+            panel1.Location = new Point(-1, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(183, 451);
+            panel1.TabIndex = 2;
+            // 
+            // buttonInicioTareas
+            // 
+            buttonInicioTareas.Location = new Point(25, 196);
+            buttonInicioTareas.Name = "buttonInicioTareas";
+            buttonInicioTareas.Size = new Size(138, 31);
+            buttonInicioTareas.TabIndex = 0;
+            buttonInicioTareas.Text = "Inicio";
+            buttonInicioTareas.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 233);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 31);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label2);
+            Controls.Add(panel1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -65,6 +90,8 @@
         #endregion
 
         private Label label1;
-        private Label label2;
+        private Panel panel1;
+        private Button buttonInicioTareas;
+        private Button button1;
     }
 }
